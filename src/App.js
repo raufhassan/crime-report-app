bae85b088b3056446ee6ed1184ef8d00c349c30d
raufhassan <raufhassan41@gmail.com>
2020-11-23 15:36:38 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Firebase, { FirebaseContext } from "./components/Firebase";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FirebaseContext.Provider value={new Firebase()}>
+      <div className="App"></div>
+    </FirebaseContext.Provider>
   );
 }
 
